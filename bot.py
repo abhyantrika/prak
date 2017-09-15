1a8a1ee083b65f23eb32b3aa0a54ed295050cbfa
abhyantrika <hjkpidiot@gmail.com>
2017-09-16 01:42:43 +0530

    hopefull final

--- bot.py
+++ bot.py
@@ -60,22 +60,7 @@ def initialize():
 	print("Bot Created..!")	
 	print('*'*78)
 
-def botty(instruction_no):
-
-	instr_dict = get_response(instruction_no,'query/')
-	keys = instr_dict.keys()
-	if 'text' in keys:
-		print(instr_dict['text'])
-		if 'options' in keys:
-			print(instr_dict['options'])
-		t = input()
-		globals()[instr_dict['var']] = t
-		variables = (instr_dict['var'],t)
-		instruction_no+=1
-	return variables	
-
-
-def botty_2(instruction_no,variables):
+def botty(instruction_no,variables,prev): #prev has success or failure of previous reply.
 	success = True
 	instr_dict = get_response(instruction_no,'query/')
 	keys = instr_dict.keys()
@@ -100,12 +85,13 @@ def botty_2(instruction_no,variables):
 			print(ins)		
 
 	if 'text' in keys:
-		print(instr_dict['text'])
+		if 'conditions' not in keys:
+			print(instr_dict['text'])
 
 		if 'options' in keys:
 			print(instr_dict['options'])
 
-		if ('var' in keys) and ('formula' not in keys):
+		if ('var' in keys) and ('formula' not in keys) and ('conditions' not in keys):
 			if instr_dict['var'] in dict(variables).keys():
 				variables.pop(-1)
 
@@ -119,20 +105,21 @@ def botty_2(instruction_no,variables):
 				#variables.append((variables[-1][0],eval(variables[-1][0]).append(t)))
 
 		if 'conditions' in keys:
-			for i in range(len(instr_dict['conditions'])):
-				for j in range(len(instr_dict['conditions'][i])):
-					instr_dict['conditions'][i][j] = eval(instr_dict['conditions'][i][j])
+			for j in range(len(instr_dict['conditions'])):
+				for k in range(len(instr_dict['conditions'][j])):
+					instr_dict['conditions'][j][k] = eval(instr_dict['conditions'][j][k])
 
-
-			for i in range(len(instr_dict['conditions'])):
+			for j in range(len(instr_dict['conditions'])):
 				if len(instr_dict['conditions'])!=1:
-					instr_dict['conditions'][i] = functools.reduce(lambda x,y:x and y,instr_dict['conditions'][i])
+					instr_dict['conditions'][j] = functools.reduce(lambda x,y:x and y,instr_dict['conditions'][j])
 			if len(instr_dict['conditions'])!=1:		
-				instr_dict['conditions'][i] = functools.reduce(lambda x,y:x or y,instr_dict['conditions'][i])	
+				instr_dict['conditions'][j] = functools.reduce(lambda x,y:x or y,instr_dict['conditions'][j])	
 
 			cond = instr_dict['conditions'][0][0]
 			if cond == True:
 				success = False
+				print(instr_dict['text'])
+				globals()[instr_dict['var']] = input()		
 
 	if 'formula' in keys:
 		globals()[instr_dict['var']] = eval(instr_dict['formula'])		
@@ -141,120 +128,24 @@ def botty_2(instruction_no,variables):
 	return variables,success		
 
 
-def main_2():
-	variables = []
-
-	for i in range(17):
-		variables,success = botty_2(i,variables)
-		while success==False:
-			variables,success = botty_2(i,variables)
-
-
 def main():
-	instruction_no = 0
-	instr_dict = get_response(instruction_no,'query/')
-	keys = instr_dict.keys()
 	variables = []
-
-	if 'instruction' in keys:
-		print(instr_dict['instruction'])
-		if len(keys)==1:
-			instruction_no+=1
-	
-	for _ in range(3):
-		variables.append(botty(instruction_no))
-		instruction_no+=1
-
-	#age
-	c = 0
+	i = 0
 	while True:
-		instr_dict = get_response(instruction_no,'query/')
-		keys = instr_dict.keys()
-		if 'text' in keys:
-			if c==0:
-				print(instr_dict['text'])
-			if 'var' in keys:
-				t = input()
-				globals()[instr_dict['var']] = t
-		instruction_no+=1
-		c+=1
-
-		instr_dict_2 = get_response(instruction_no,'query/')
-		cond = instr_dict_2['conditions'][0][0]
-		if not eval(cond):
-			break
-		else:
-			instruction_no-=1
-			print(instr_dict_2['text'])
-			continue	
-	variables.append((instr_dict['var'],t))	
-	instruction_no+=1		
-	
-	instr_dict = get_response(instruction_no,'query/')		
-	print(instr_dict['instruction'])
-	instruction_no+=1
-
+		try:
+			variables,success = botty(i,variables,True)
+			while success==False:
+				variables,success = botty(i,variables,False)
+			i+=1	
+		except:
+			break		
 
 	data = {}
 	for v in variables:
 		if v[0] in ['first_name','last_name','gender','age']:
 			data[v[0]] = v[1]
 	post_response('details/',data)
-	
-
-
-	#name and stuff
-	try:
-		instr_dict = get_response(instruction_no,'query/')		
-		keys = instr_dict.keys()
-		globals()[instr_dict['var']] = eval(instr_dict['formula'])
-		instruction_no+=1
-		variables.append((instr_dict['var'],eval(instr_dict['formula'])))
-		
-
-
-		instr_dict = get_response(instruction_no,'query/')		
-		keys = instr_dict.keys()
-		if 'instruction_var' in keys:
-			print(instr_dict['instruction']%(variables[-1][1]))
-		instruction_no+=1	
-
-		#matrix setup
-		instr_dict = get_response(instruction_no,'query/')		
-		keys = instr_dict.keys()
-		globals()[instr_dict['var']] = eval(instr_dict['formula'])
-		instruction_no+=1
-		v = instr_dict['var']
-
-		for _ in range(3):
-			instr_dict = get_response(instruction_no,'query/')		
-			keys = instr_dict.keys()
-			print(instr_dict['text'])
-			t = input()
-			eval(v).append(t)
-			instruction_no+=1
-		variables.append((v,eval(v)))		
-		
-		#formula and transpose
-		for _ in range(2):
-			instr_dict = get_response(instruction_no,'query/')
-			globals()[instr_dict['var']] = eval(instr_dict['formula'])
-			variables.append((instr_dict['var'],eval(instr_dict['formula'])))
-			instruction_no+=1
-
-		instr_dict = get_response(instruction_no,'query/')		
-		print(instr_dict['instruction'])
-		instruction_no+=1	
-
-		#Display transpose
-		instr_dict = get_response(instruction_no,'query/')	
-		for i in range(int(instr_dict['list_length'])):
-			a = eval(instr_dict['instruction_var'][0])
-			b = eval(instr_dict['instruction_var'][1])
-			print(instr_dict['instruction']%(a,b))
-	except:
-		pass		
-	print("GoodBye!")		
+	print("GoodBye")		
 
 def delete():
 	response = requests.get(API_END+'delete/')#,auth=HTTPBasicAuth(username,API_KEY))
@@ -262,5 +153,5 @@ def delete():
 
 if __name__ == '__main__':
 	initialize()
-	main_2()
+	main()
 	delete()
